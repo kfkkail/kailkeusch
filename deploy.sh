0db5e3ecd430a0fa7b9ad1c036282453a6241adc
Kail Keusch <kfkkail@gmail.com>
2016-11-05 18:52:33 -0400

    Added all files to deploy script
    Deleted the separate deploy and now the one script deploys everything.

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,5 @@
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/kail/ /users/kfkkail/Github/kailkeusch/css/
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/kail/ /users/kfkkail/Github/kailkeusch/index.html
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/kail/ /users/kfkkail/Github/kailkeusch/missing.html
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/css/
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/lib/
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/img/
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/index.html
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/missing.html
--- deploy_lib.sh
+++
@@ -1,1 +0,0 @@
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/lib/
