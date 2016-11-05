4a39f623ec060c6ec2fc0027bd7640c0d5e114ed
Kail Keusch <kfkkail@gmail.com>
2016-11-05 18:45:18 -0400

    Changed deploy script for my mac

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/js/
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/css/
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/index.html
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /home/kfkkail/Documents/git/andreaandkail/missing.html
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/kail/ /users/kfkkail/Github/kailkeusch/css/
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/kail/ /users/kfkkail/Github/kailkeusch/index.html
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/kail/ /users/kfkkail/Github/kailkeusch/missing.html
