9d256fb95ad5896fea37d1b8bfa965c65d575678
Kail Keusch <kfkkail@gmail.com>
2016-11-07 22:50:26 -0500

    adjusted deploy script to push to main site

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/css/
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/lib/
-ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/img/
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/index.html
-ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../kail/ /users/kfkkail/Github/kailkeusch/missing.html
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /users/kfkkail/Github/kailkeusch/css/
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /users/kfkkail/Github/kailkeusch/lib/
+ncftpput -R -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /users/kfkkail/Github/kailkeusch/img/
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /users/kfkkail/Github/kailkeusch/index.html
+ncftpput -v -u "keuschfamily" -p "Walnut18" ftp.nc1.server313.com ../www/ /users/kfkkail/Github/kailkeusch/missing.html
